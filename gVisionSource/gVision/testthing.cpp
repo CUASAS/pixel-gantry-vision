c527c2b1d640309ed112f9629c5133049bd6f49c
Hayden Swanson <70072888+hswanson13@users.noreply.github.com>
2020-10-27 21:37:41 -0500

    Crosshair detection paramaterized and set up of the Labview User Interface for crosshair detection

--- gVisionSource/gVision/testthing.cpp
+++ gVisionSource/gVision/testthing.cpp
@@ -1,4 +1,3 @@
-
 #include "stdafx.h"
 #include "utils.h"
 #include "testthing.h"
@@ -17,12 +16,12 @@ void show2(cv::Mat img)
 	cv::destroyWindow("MyWindow");
 }
 
-vector<cv::Vec3f> get_circles(cv::Mat& img, int min_radius, int max_radius)
+vector<cv::Vec3f> get_circles(cv::Mat& img, int min_radius, int max_radius, int circle_detection_sensitivity, int CDparam_1, int CDblue, int CDgreen, int CDred, int line_thickness)
 {
-
 	cv::GaussianBlur(img, img, cv::Size(5, 5), 0);
-	vector<cv::Vec3f> circle_vector;     //idk if this works hope so, also might be bad code
-	cv::HoughCircles(img, circle_vector, cv::HOUGH_GRADIENT, 1, img.rows / 16, 100, 30, min_radius, max_radius); //paramaters wierd??
+	vector<cv::Vec3f> circle_vector;     
+	cv::HoughCircles(img, circle_vector, cv::HOUGH_GRADIENT, 1, img.rows / 16, CDparam_1, circle_detection_sensitivity, min_radius, max_radius); //paramaters wierd??
+	//Hough Cirlces Paramaters
 	/*
 cv::HoughCircles(img, circles, cv::HOUGH_GRADIENT, 1, img.rows / 16, 100, 30, 1, 30);
 gray: Input image(grayscale).
@@ -34,6 +33,13 @@ param_1 = 200 : Upper threshold for the internal Canny edge detector.
 param_2 = 100 * : Threshold for center detection.
 min_radius = 0 : Minimum radius to be detected.If unknown, put zero as default.
 max_radius = 0 : Maximum radius to be detected.If unknown, put zero as default.
+
+param1	first method-specific parameter. In case of HOUGH_GRADIENT , it is the higher threshold of the two passed to the Canny edge detector (the lower one is twice smaller).
+
+param2	second method-specific parameter. In case of HOUGH_GRADIENT , 
+it is the accumulator threshold for the circle centers at the detection stage. 
+The smaller it is, the more false circles may be detected. 
+Circles, corresponding to the larger accumulator values, will be returned first.
 */
 	if (!circle_vector.empty()) //otherwise draw circle
 	{
@@ -42,44 +48,60 @@ max_radius = 0 : Maximum radius to be detected.If unknown, put zero as default.
 			cv::Vec3i c = circle_vector[i];
 			cv::Point center = cv::Point(c[0], c[1]);
 			// circle center
-			cv::circle(img, center, 1, cv::Scalar(0, 100, 100), 3, cv::LINE_AA);
+			cv::circle(img, center, 1, cv::Scalar(CDblue, CDgreen, CDred), 1, cv::LINE_AA);
+			//img, center, radius, color(R,G,B), line thickness
 			// circle outline
 			int radius = c[2];
-			circle(img, center, radius, cv::Scalar(255, 0, 255), 3, cv::LINE_AA);
+			circle(img, center, radius, cv::Scalar(CDblue, CDgreen, CDred), line_thickness, cv::LINE_AA);
+
 		}
 	}
 	return circle_vector;
 }
-//it changes the mat so u dont have to return 
+//it changes the mat so u dont have to return the image
 
-__declspec(dllexport) int __cdecl helloworld(char* imgPtr, int imgLineWidth, int imgWidth, int imgHeight, int min_radius, int max_radius)
+//Point paramaters
+/*(InputOutputArray 	img,
+	Point 	center,
+	int 	radius,
+	const Scalar& color,
+	int 	thickness = 1,
+	int 	lineType = LINE_8,
+	int 	shift = 0
+	)
+	*/
+
+
+void error_circle(cv::Mat& img)
 {
-	double percent_size = .4;
+	//get width of picture(scaled)
+	//set x_fov and error bound to be whatever you want
+	//PARAMATERIZE
+}
+
+
+__declspec(dllexport) int __cdecl helloworld(char* imgPtr, int imgLineWidth, int imgWidth, int imgHeight, 
+					double percent_size, int min_radius, int max_radius, int circle_detection_sensitivity, int CDparam_1, int CDblue, int CDgreen, int CDred, int line_thickness)
+{
 	cv::Mat img(imgHeight, imgWidth, CV_8U, (void*)imgPtr, imgLineWidth);
+	
 	cv::resize(img, img, cv::Size(img.cols * percent_size, img.rows * percent_size), 0, 0);
 
-	get_circles(img, min_radius, max_radius);
+	get_circles(img, min_radius, max_radius, circle_detection_sensitivity, CDparam_1,CDblue, CDgreen, CDred, line_thickness);
 
-	//Houghcirlces
-	//use houghcircle info to draw houghcircles
-	//return that info (circle,x,y)
+	//error python outline? 
+	/*
+	error_bound = 10 # 10 micrometers
+	 #we got width/length of picture in pixels earlier
+	 x_fov = 2000 # 2mm or 2000 micrometers
+	 #or if FOV is area then np.sqrt(length_field)
+	 pixel_length = x_fov/xpixel #this tells you in x_fov there are this many xpixels
+	 r_error = error_bound / pixel_length #error / pixel length gets you the "radius of error"
+	 error_circle = cv2.circle(image, (x,y), int(r_error), 255, 1)*/
 
 	show2(img);
 	return 0;
 }
-
-/*
-cv::HoughCircles(img, circles, cv::HOUGH_GRADIENT, 1, img.rows / 16, 100, 30, 1, 30);
-gray: Input image(grayscale).
-circles : A vector that stores sets of 3 values : xc, yc, r for each detected circle.
-HOUGH_GRADIENT : Define the detection method.Currently this is the only one available in OpenCV.
-dp = 1 : The inverse ratio of resolution.
-min_dist = gray.rows / 16 : Minimum distance between detected centers.
-param_1 = 200 : Upper threshold for the internal Canny edge detector.
-param_2 = 100 * : Threshold for center detection.
-min_radius = 0 : Minimum radius to be detected.If unknown, put zero as default.
-max_radius = 0 : Maximum radius to be detected.If unknown, put zero as default.
-*/
 
 //c++ code changes
 //compile code (build solution)
--- gVisionSource/gVision/testthing.h
+++ gVisionSource/gVision/testthing.h
@@ -1,6 +1,7 @@
 #pragma once
 
-extern "C" __declspec(dllexport) int __cdecl helloworld(char* imgPtr, int imgLineWidth, int imgWidth, int imgHeight, int min_radius, int max_radius);
+extern "C" __declspec(dllexport) int __cdecl helloworld(char* imgPtr, int imgLineWidth, int imgWidth, int imgHeight,
+	double percent_size, int min_radius, int max_radius, int circle_detection_sensitivity, int CDparam_1, int CDblue, int CDgreen, int CDred, int line_thickness);
 
 
 //if ur func has arguments pass those too
